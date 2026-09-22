4bfa2c0a9811b11633f93b7b862d35d60edb9a81
yousef856 <47804849+yousef856@users.noreply.github.com>
2026-09-22 22:39:34 +0300

    Enhance Footer component with currentYear property
    Added currentYear property to Footer component.

--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -2,8 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
+  standalone: true,
   imports: [],
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
-export class Footer {}
+export class Footer {
+  currentYear = new Date().getFullYear();
+                    }
